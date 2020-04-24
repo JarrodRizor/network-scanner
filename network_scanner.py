c2e1c088a75fee1f92f1249fa0b51ac609bfff8c
Jarrod Rizor <me@jarrodrizor.com>
2020-04-24 13:35:14 -0400

    Pulling arp broadcast requests into a answered list and looping through that list to abstract the IP address and MAC address of sources on the network.

--- network_scanner.py
+++ network_scanner.py
@@ -4,6 +4,13 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    
+    for element in answered:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("--------------------------------")
 
-scan("192.168.1.1/24")
+scan("192.168.1.67/24")
