c5a1e7eeaf54582a7fadedcfff7580e3265c343c
Jarrod Rizor <me@jarrodrizor.com>
2020-04-24 15:38:14 -0400

    refactoring code to abstract out printing tabular data into it's own function and feeding the results from scan(ip) into the print_results to display the desired output.

--- network_scanner.py
+++ network_scanner.py
@@ -2,14 +2,27 @@
 
 import scapy.all as scapy
 
+# Scans network for ARP requests
+# returns list of data found after scapy scans the network
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     
-    print("IP\t\t\tMAC Address\n-------------------------------------")
+    client_list = []
     for element in answered:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
 
-scan("192.168.1.67/24")
+# Print the output of the results gathered in the ARP scan
+# Loop through the results and display the results in a table 
+# (for IP address and Mac Address)
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+scan_result = scan("192.168.1.67/24")
+print_result(scan_result)
